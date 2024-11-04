bb4287c00d1aebd2f6231186c1721d6bc4ec41cf
ethanenger <engerethan@gmail.com>
2024-11-04 13:22:42 -0700

    logging in

---
+++ src/features/authentication/services/auth-provider.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
+
+const oidcConfig: AuthProviderProps = {
+  authority: "https://auth.snowse.duckdns.org/realms/advanced-frontend/",
+  client_id: "ethan-class-demo",
+  redirect_uri: "http://localhost:5173/" ,
+  onSigninCallback: async (user) => {
+    console.log("Signed in callback");
+    const newUrl = window.location.href.split("?")[0];
+    window.history.replaceState({}, document.title, newUrl);
+
+    console.log(user?.access_token);
+    const expiresDate = new Date(user!.expires_at! * 1000).toUTCString();
+    document.cookie = `jwt_token=${user?.access_token}; expires=${expiresDate};`;
+  },
+  onRemoveUser: () => {
+    document.cookie = `jwt_token= ; expires=${new Date(0)};`;
+  }
+};
+
+export const CustomAuthProvider = ({ children }: { children: ReactNode }) => {
+  return <AuthProvider {...oidcConfig}>{children}</AuthProvider>;
+};
---
+++ src/features/authentication/ui/login-button.tsx
@@ -0,0 +1,43 @@
+import { useAuth } from "react-oidc-context";
+
+export default function LoginLogoutButton() {
+  const auth = useAuth();
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div className="text-zinc-50">Signing you in...</div>;
+    case "signoutRedirect":
+      return <div className="text-zinc-50">Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div className="text-zinc-50">Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div className="text-zinc-50">Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <button
+        className="text-zinc-300 hover:bg-zinc-700 hover:text-zinc-50 px-3 py-2 rounded-md text-sm font-medium"
+        onClick={() => {
+          document.cookie = "jwt_token=;";
+          auth.removeUser();
+        }}
+      >
+        Log out
+      </button>
+    );
+  }
+
+  return (
+    <button
+      className="text-zinc-300 hover:bg-zinc-700 hover:text-zinc-50 px-3 py-2 rounded-md text-sm font-medium"
+      onClick={() => void auth.signinRedirect()}
+    >
+      Log in
+    </button>
+  );
+}
