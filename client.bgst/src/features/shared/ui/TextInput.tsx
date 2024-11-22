832255070c49c860461a9b4fe6bc6ba08f03b570
ethanenger <engerethan@gmail.com>
2024-11-22 09:19:37 -0700

    disabled input

--- client.bgst/src/features/shared/ui/TextInput.tsx
+++ client.bgst/src/features/shared/ui/TextInput.tsx
@@ -7,7 +7,7 @@ export const TextInput = ({input, setInput, placeholder, disabled, className}: {
             disabled={disabled}
             onChange={(e) => setInput(e.target.value)}
             placeholder={placeholder}
-            className={`w-full h-full p-2  bg-inherit border-swhite-300 shadow-sm ring-inset outline-inherit ${className}`}
+            className={`w-full h-full p-2  bg-inherit border-swhite-300 shadow-sm ring-inset outline-inherit ${className} disabled:bg-swhite-100 disabled:text-swhite-700`}
           />
   )
 }
